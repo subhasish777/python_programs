0893365ab253c19bff6662e39a78d2e327312c76
subhasish777 <57346327+subhasish777@users.noreply.github.com>
2025-09-17 20:21:41 +0530

    Add files via upload
    Tute Dude Module1 python programs

---
+++ Task 2 Create a Personalized Greeting.py
@@ -0,0 +1,4 @@
+firstname = input("enter first name")
+lastname = input("enter last name")
+
+print("Hello "+ firstname +' '+lastname+"!" +" Welcome to Pyhton programming")
---
+++ assignement1.py
@@ -0,0 +1,14 @@
+a=input("Enter the first number")
+a=int(a)
+b=input("Enter the second number")
+b=int(b)
+
+Addition=a+b
+substraction=a-b
+division=a/b
+multiplication=a*b
+
+print("Addition = " ,Addition)
+print("Substraction = ",substraction)
+print("Division = ",division)
+print("Multiplication = ",multiplication)
